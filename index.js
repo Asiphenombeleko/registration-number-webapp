b7df47a4ed9e0392c8ccd65e8ce6c8880b11ee6f
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-08-30 20:14:02 +0200

    created file setup and also imported modules

---
+++ index.js
@@ -0,0 +1,38 @@
+//import all my node modules
+import express from 'express'
+import bodyParser from 'body-parser'
+import { engine } from 'express-handlebars/types'
+import session from 'express-session'
+import flash from 'express-flash'
+
+const app = express()
+//create an instance for my database function
+
+app.engine(
+    "handlebars",
+    engine({
+      layoutsDir: "./views/layouts",
+    })
+  );
+  app.set("view engine", "handlebars");
+  app.set("views", "./views");
+  app.use(express.static("public"));
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  
+  app.use(bodyParser.json());
+  
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: "Asiphe's",
+    })
+  );
+  // session middleware
+  app.use(flash());
+
+const PORT = process.env.PORT || 3012;
+app.listen(PORT, ()=>{
+    console.log("listen at localhost:", PORT);
+})
---
+++ regfactory.js
@@ -0,0 +1,12 @@
+export default function Registrations(){
+    async function getReg(regNo){
+
+    }
+    async function displayReg(){
+        return getReg
+    }
+    return{
+        getReg,
+        displayReg
+    }
+}
