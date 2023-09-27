de9f64c64bc40e6dffda7dbab6834c520ffd9bab
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-09-27 16:24:20 +0200

    added the URL

--- test/reg.test.js
+++ test/reg.test.js
@@ -7,23 +7,10 @@ const connectionString = process.env.DATABASE_URL || "postgresql://codex:xcode12
 
 const db = pgPromise()(connectionString);
 
-// describe('Registration Data', function () {
-//   this.timeout(20000)
 
-//   beforeEach(async function () {
-//     await db.none('DELETE FROM registration_table')
-
-//   })
-  // it('should insert registration data into the database', async () => {
-  //   const regNo = 'CA 123 123';
-  //     const regId = 1
-  //     await data.getTownId(regId)
-
-  //   let regData  = await data.insertRegData(regNo)
-
-  //   assert.deepEqual(regData.regNo, 'CA 123 123')
-  // });
   describe('insertRegData', () => {
+    
+    
     let data = database(db)
     it('should insert registration data into the database', async () => {
       const townTag = 'CA';
